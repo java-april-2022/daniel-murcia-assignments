2e13e8c482e90462aa8e5d140a1cea8ebea68ef2
dm2800 <danielmurcia@me.com>
2022-04-01 19:45:47 -0700

    initial commit

---
+++ Week1/First_Java_Program/FirstJavaProgram.java
@@ -0,0 +1,9 @@
+
+
+public class FirstJavaProgram {
+    public static void main(String[] args) {
+        System.out.println("My name is Danny Murcia");
+        System.out.println("I am 30 years old");
+        System.out.println("My hometown is Los Angeles, CA");
+    }
+}
